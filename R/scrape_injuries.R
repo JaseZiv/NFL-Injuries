d269e8bd00797c5b5f573fc806f589899d1f2ab1
JaseZiv <jaseziv83@gmail.com>
2022-09-05 21:19:01 +1000

    initial commit

---
+++ R/scrape_injuries.R
@@ -0,0 +1,63 @@
+library(dplyr)
+library(rvest)
+library(httr)
+library(stringr)
+
+###########################################################################
+# Scraper -----------------------------------------------------------------
+# this scraper gets both injured players, and players placed on IL (two
+# checkboxes are selected on the main_link)
+###########################################################################
+
+# code to get the URLs of each page of data
+main_link <- "http://www.prosportstransactions.com/football/Search/"
+first_link <- "https://www.prosportstransactions.com/football/Search/SearchResults.php?Player=&Team=&BeginDate=&EndDate=&InjuriesChkBx=yes&submit=Search"
+
+link_nodes <- read_html(first_link) %>% html_nodes(".bodyCopy a")
+
+each_link <- link_nodes %>% html_attr("href")
+each_link <- paste0(main_link, each_link)
+
+
+all_links <- c(first_link, each_link) %>% unique()
+
+page_number <- c(1, link_nodes %>% html_text())
+
+# start scrape:
+all_injuries <- data.frame()
+
+for (i in 1:length(all_links)) {
+  print(paste("scraping page:", all_links[i]))
+  # url <- read_html(all_links[i])
+
+  history <- httr::RETRY("GET",
+                         url = all_links[i],
+                         times = 5, # the function has other params to tweak its behavior
+                         pause_min = 5,
+                         pause_base = 2)
+
+  history <- history %>% httr::content()
+
+  each_page_df <- history %>% html_nodes(".datatable") %>% html_table() %>% data.frame()
+
+  # each_page_df <- url %>% html_nodes(".datatable") %>% html_table() %>% data.frame()
+
+  columnNames <- each_page_df[1,] %>% as.character()
+
+  colnames(each_page_df) <- columnNames
+  each_page_df <- each_page_df[-1,]
+
+  each_page_df <- each_page_df %>% mutate_all(as.character)
+  each_page_df$page_url <- all_links[i]
+  each_page_df$page_num <- page_number[i]
+
+  all_injuries <- bind_rows(all_injuries, each_page_df)
+
+}
+
+# remove any duplicates
+all_injuries <- all_injuries %>%
+  distinct(Date, Team, Acquired, Relinquished, Notes, .keep_all = T)
+
+# write final file
+saveRDS(all_injuries, here::here("data", "nfl_injuries_complete_raw.rds"))
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # NFL-Injuries
 Scraper to get NFL injuries from prosporttransactions.com
+
+
+## What is this project
+
+Much like the scraper written for NBA injuries, this project gets NFL injurieds from the popular prosportstransactions.com website.
+
+The data is then injested by the popular hashtagfootball.com website tyo use in Fantasy Football tools.
